10dbfa148d083139703b325311ce45ef1a5fe1a4
Hidea <adelatour11@yahoo.fr>
2024-04-27 10:39:56 +0200

    Update TMDB.py

--- TMDB.py
+++ TMDB.py
@@ -12,7 +12,7 @@ url = "https://api.themoviedb.org/3/"
 # Set your TMDB API Read Access Token key here
 headers = {
     "accept": "application/json",
-    "Authorization": "Bearer XXX"
+    "Authorization": "Bearer XXXX"
 }
 # The font used
 truetype_url = 'https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto-Light.ttf'
@@ -21,38 +21,31 @@ truetype_url = 'https://github.com/googlefonts/roboto/raw/main/src/hinted/Roboto
 trending_movies_url = f'{url}trending/movie/week?language=en-US'
 trending_tvshows_url = f'{url}trending/tv/week?language=en-US'
 
-# Fetching trending movies
-trending_movies_response = requests.get(trending_movies_url, headers=headers)
-trending_movies = trending_movies_response.json()
+# Fetching trending movies and TV shows
+def fetch_trending(url):
+    """Fetches trending movies or TV shows"""
+    response = requests.get(url, headers=headers)
+    return response.json().get('results', [])
 
-# Fetching trending TV shows
-trending_tvshows_response = requests.get(trending_tvshows_url, headers=headers)
-trending_tvshows = trending_tvshows_response.json()
+trending_movies = fetch_trending(trending_movies_url)
+trending_tvshows = fetch_trending(trending_tvshows_url)
 
-# Fetching genres for movies
-genres_url = f'{url}genre/movie/list?language=en-US'
-genres_response = requests.get(genres_url, headers=headers)
-genres_data = genres_response.json()
-movie_genres = {genre['id']: genre['name'] for genre in genres_data.get('genres', [])}
+# Fetching genres for movies and TV shows
+def fetch_genres(media_type):
+    """Fetches genres for movies or TV shows"""
+    genres_url = f'{url}genre/{media_type}/list?language=en-US'
+    response = requests.get(genres_url, headers=headers)
+    return {genre['id']: genre['name'] for genre in response.json().get('genres', [])}
 
-# Fetching genres for TV shows
-genres_url = f'{url}genre/tv/list?language=en-US'
-genres_response = requests.get(genres_url, headers=headers)
-genres_data = genres_response.json()
-tv_genres = {genre['id']: genre['name'] for genre in genres_data.get('genres', [])}
+movie_genres = fetch_genres("movie")
+tv_genres = fetch_genres("tv")
 
-# Fetching TV show details
-def get_tv_show_details(tv_id):
-    tv_details_url = f'{url}tv/{tv_id}?language=en-US'
-    tv_details_response = requests.get(tv_details_url, headers=headers)
-    return tv_details_response.json()
-
-# Fetching movie details
-def get_movie_details(movie_id):
-    movie_details_url = f'{url}movie/{movie_id}?language=en-US'
-    movie_details_response = requests.get(movie_details_url, headers=headers)
-    return movie_details_response.json()
-
+# Fetching TV show and movie details
+def fetch_details(media_type, media_id):
+    """Fetches details of a movie or TV show"""
+    details_url = f'{url}{media_type}/{media_id}?language=en-US'
+    response = requests.get(details_url, headers=headers)
+    return response.json()
 
 # Create a directory to save the backgrounds
 background_dir = "tmdb_backgrounds"
@@ -61,104 +54,92 @@ if os.path.exists(background_dir):
     shutil.rmtree(background_dir)
 os.makedirs(background_dir, exist_ok=True)
 
-#truncate overview
-def truncate_overview(overview, max_chars):
-    if len(overview) > max_chars:
-        return overview[:max_chars]
-    else:
-        return overview
-
-#truncate
-def truncate(overview, max_chars):
-    if len(overview) > max_chars:
-        return overview[:max_chars-3]
-    else:
-        return overview
-
-# resize image
+# Resize image
 def resize_image(image, height):
+    """Resizes an image"""
     ratio = height / image.height
     width = int(image.width * ratio)
     return image.resize((width, height))
 
+# Clean filename
 def clean_filename(filename):
-    # Remove problematic characters from the filename
-    cleaned_filename = "".join(c if c.isalnum() or c in "._-" else "_" for c in filename)
-    return cleaned_filename
+    """Cleans a filename"""
+    return "".join(c if c.isalnum() or c in "._-" else "_" for c in filename)
 
+# Process image
 def process_image(image_url, title, is_movie, genre, year, rating, duration=None, seasons=None):
-    # Download the background image with a timeout of 10 seconds
+    """
+    Processes and saves an image.
+
+    Parameters:
+        image_url (str): URL of the image.
+        title (str): Title of the movie or TV show.
+        is_movie (bool): True if the media is a movie, False if it's a TV show.
+        genre (str): Genre of the movie or TV show.
+        year (str): Release year of the movie or TV show.
+        rating (float): Rating of the movie or TV show.
+        duration (str, optional): Duration of the movie. Defaults to None.
+        seasons (int, optional): Number of seasons of the TV show. Defaults to None.
+    """
     response = requests.get(image_url, timeout=10)
     if response.status_code == 200:
-        # Open the image
+        # Open the downloaded image
         image = Image.open(BytesIO(response.content))
-
-        # Resize the image to have a width of 1500 pixels while preserving aspect ratio
+        # Resize the image
         image = resize_image(image, 1500)
-
-        # Open overlay images
+        # Open background, overlay, and TMDB logo images
         bckg = Image.open(os.path.join(os.path.dirname(__file__), "bckg.png"))
         overlay = Image.open(os.path.join(os.path.dirname(__file__), "overlay.png"))
         tmdblogo = Image.open(os.path.join(os.path.dirname(__file__), "tmdblogo.png"))
-
-
-        # Paste images
+        
+        # Paste the image onto the background
         bckg.paste(image, (1175, 0))
         bckg.paste(overlay, (1175, 0), overlay)
         bckg.paste(tmdblogo, (680, 975), tmdblogo)
-
-        # Add title text with shadow
         draw = ImageDraw.Draw(bckg)
-
-        #Text font
+        
+        # Define fonts and colors
         font_title = ImageFont.truetype(urlopen(truetype_url), size=190)
         font_overview = ImageFont.truetype(urlopen(truetype_url), size=45)
-        font_custom = ImageFont.truetype(urlopen(truetype_url), size=60)                 
-
-        #Text color
+        font_custom = ImageFont.truetype(urlopen(truetype_url), size=60)
         shadow_color = "black"
         main_color = "white"
-        overview_color = (150,150,150)  # Grey color for the summary
+        overview_color = (150,150,150)
         metadata_color = "white"
-
-        #Text position
+        shadow_offset = 2
+        
+        # Define text positions
         title_position = (200, 540)
         overview_position = (210, 830)
-        shadow_offset = 2
         info_position = (210, 520)
         custom_position = (210, 950)
 
-        # Draw Title for info
+        # Draw title
         draw.text((title_position[0] + shadow_offset, title_position[1] + shadow_offset), title, font=font_title, fill=shadow_color)
         draw.text(title_position, title, font=font_title, fill=main_color)
-
-        # Wrap Overview text
-        wrapped_overview = "\n".join(textwrap.wrap(overview, width=95)) + "..."
-
-        # Draw Overview for info
+        
+        # Wrap overview text
+        wrapped_overview = "\n".join(textwrap.wrap(overview,width= 90,initial_indent= "",subsequent_indent= "",expand_tabs= True,tabsize= 8,replace_whitespace= True,fix_sentence_endings= False,break_long_words= True,break_on_hyphens= True,drop_whitespace= True,max_lines= 2,placeholder= " ..."))
+        
+        # Draw overview
         draw.text((overview_position[0] + shadow_offset, overview_position[1] + shadow_offset), wrapped_overview, font=font_overview, fill=shadow_color)
         draw.text(overview_position, wrapped_overview, font=font_overview, fill=overview_color)
-
+        
         # Determine genre text and additional info
-        if is_movie:
-            genre_text = genre
-            additional_info = f"{duration}"
-        else:
-            genre_text = genre
-            additional_info = f"{seasons} {'Season' if seasons == 1 else 'Seasons'}"
-
-        rating_text = "TMDB: "+ str(rating)
-        year_text = truncate(str(year),7) 
+        genre_text = genre
+        additional_info = duration if is_movie else f"{seasons} {'Season' if seasons == 1 else 'Seasons'}"
+        rating_text = f"TMDB: {rating}"
+        year_text = year[:7]
         info_text = f"{genre_text}  •  {year_text}  •  {additional_info}  •  {rating_text}"
-
+        
         # Draw metadata
         draw.text((info_position[0] + shadow_offset, info_position[1] + shadow_offset), info_text, font=font_overview, fill=shadow_color)
         draw.text(info_position, info_text, font=font_overview, fill=overview_color)
-
+        
         # Draw custom text
-        draw.text((custom_position[0] + shadow_offset, custom_position[1] + shadow_offset), custom_text, font=font_custom, fill=shadow_color)                  
-        draw.text(custom_position, custom_text, font=font_custom, fill=metadata_color)
-
+        draw.text((custom_position[0] + shadow_offset, custom_position[1] + shadow_offset), "Now Trending on", font=font_custom, fill=shadow_color)
+        draw.text(custom_position, "Now Trending on", font=font_custom, fill=metadata_color)
+        
         # Save the resized image
         filename = os.path.join(background_dir, f"{clean_filename(title)}.jpg")
         bckg = bckg.convert('RGB')
@@ -167,45 +148,59 @@ def process_image(image_url, title, is_movie, genre, year, rating, duration=None
     else:
         print(f"Failed to download background for {title}")
 
+
 # Process each trending movie
-for movie in trending_movies.get('results', []):
+for movie in trending_movies:
+    # Extract movie details
     title = movie['title']
-    overview = truncate_overview(movie['overview'],175)
+    overview = movie['overview']
     year = movie['release_date']
-    rating = round(movie['vote_average'],1)
+    rating = round(movie['vote_average'], 1)
     genre = ', '.join([movie_genres[genre_id] for genre_id in movie['genre_ids']])
-
-
-    movie_details = get_movie_details(movie['id'])
-    duration = movie_details.get('runtime', 0)
-    if duration:
+    
+    # Fetch additional movie details
+    movie_details = fetch_details("movie", movie['id'])
+    duration = movie_details.get('runtime', "N/A")
+    
+    # Format duration as hours and minutes
+    if duration != "N/A":
         hours = duration // 60
         minutes = duration % 60
         duration = f"{hours}h{minutes}min"
-    else:
-        duration = "N/A"
-
+        
+    # Check if backdrop image is available
     backdrop_path = movie['backdrop_path']
-    custom_text = "Now Trending on"
     if backdrop_path:
+        # Construct image URL
         image_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
+        
+        # Process the image
         process_image(image_url, title, is_movie=True, genre=genre, year=year, rating=rating, duration=duration)
     else:
+        # Print error message if no backdrop image found
         print(f"No backdrop image found for {title}")
 
 # Process trending TV shows
-for tvshow in trending_tvshows.get('results', []):
-    title = truncate_overview(tvshow['name'],38)
-    overview = truncate_overview(tvshow['overview'],175)
+for tvshow in trending_tvshows:
+    # Extract TV show details
+    title = textwrap.shorten(tvshow['name'], width=38)
+    overview = textwrap.shorten(tvshow['overview'], width=175)
     year = tvshow['first_air_date']
-    rating = round(tvshow['vote_average'],1)
+    rating = round(tvshow['vote_average'], 1)
     genre = ', '.join([tv_genres[genre_id] for genre_id in tvshow['genre_ids']])
-    tv_details = get_tv_show_details(tvshow['id'])
+    
+    # Fetch additional TV show details
+    tv_details = fetch_details("tv", tvshow['id'])
     seasons = tv_details.get('number_of_seasons', 0)
+    
+    # Check if backdrop image is available
     backdrop_path = tvshow['backdrop_path']
-    custom_text = "Now Trending on"
     if backdrop_path:
+        # Construct image URL
         image_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
+        
+        # Process the image
         process_image(image_url, title, is_movie=False, genre=genre, year=year, rating=rating, seasons=seasons)
     else:
+        # Print error message if no backdrop image found
         print(f"No backdrop image found for {title}")
